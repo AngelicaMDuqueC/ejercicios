d4b34ebc464567faea5727bd57821cefd81bed92
Angélica María D <angelidu@gmail.com>
2017-02-20 18:25:34 -0500

    correccion del ejercicio

--- practic2.rb
+++ practic2.rb
@@ -1,6 +1,5 @@
 zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
 #Para mantenernos organizados, organiza el array zombie_apocalypse_supplies de forma alfabetica.
-puts
-puts zombie_apocalypse_supplies.sort
+puts zombie_apocalypse_supplies.sort_by {|x| x.downcase}
 puts
 puts "-"*20
